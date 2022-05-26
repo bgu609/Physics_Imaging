26dfdeb5425b7ec489f6d743119ab5f03a4bd850
bgw <giung609@naver.com>
2022-05-26 11:31:50 +0900

    이 패턴으론 particle 4000 ~ 5000 정도를 한계치로 보는 것이 맞을듯

--- PHYSICS/IdealGas_Simulator/ViewModels/DrawingSpace.cs
+++ PHYSICS/IdealGas_Simulator/ViewModels/DrawingSpace.cs
@@ -34,58 +34,7 @@ namespace IdealGas_Simulator.ViewModels
 
         public void Now_Rendering_Particles(List<PixelParticle> Particle_List)
         {
-            //Application.Current.Dispatcher.Invoke(() => {
-            //    BitMap.Lock();
-            //});
-            
-            //Clear_Particles();
-            //await Async_Rendering(Particle_List);
-            //Task.Run(() => Sync_Rendering(Particle_List));
-            Sync_Rendering(Particle_List);
-
-            //Application.Current.Dispatcher.Invoke(() => {
-            //    BitMap.Unlock();
-            //});
-        }
-
-        private async Task Async_Rendering(List<PixelParticle> Particle_List)
-        {
-            List<PixelParticle> Collector = Particle_List.ToList();
-            List<Task> Task_List = new List<Task>() // Boundary 그리기
-            {
-                Task.Run(() => {
-                    Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Send, new Action(() => {
-                        WriteableBitmapExtensions.DrawLineAa(BitMap, 800, 100, 1600, 100, Color.FromArgb(0xFF, 0xFF, 0xFF, 0xFF), 20);
-                    }));
-                }),
-                Task.Run(() => {
-                    Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Send, new Action(() => {
-                        WriteableBitmapExtensions.DrawLineAa(BitMap, 1600, 100, 1600, 1400, Color.FromArgb(0xFF, 0xFF, 0xFF, 0xFF), 20);
-                    }));
-                }),
-                Task.Run(() => {
-                    Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Send, new Action(() => {
-                        WriteableBitmapExtensions.DrawLineAa(BitMap, 1600, 1400, 800, 1400, Color.FromArgb(0xFF, 0xFF, 0xFF, 0xFF), 20);
-                    }));
-                }),
-                Task.Run(() => {
-                    Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Send, new Action(() => {
-                        WriteableBitmapExtensions.DrawLineAa(BitMap, 800, 1400, 800, 100, Color.FromArgb(0xFF, 0xFF, 0xFF, 0xFF), 20);
-                    }));
-                }),
-            };
-
-            foreach (PixelParticle item in Collector) // Particles 그리기
-            {
-                Task_List.Add(Task.Run(() => {
-                    Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
-                    {
-                        WriteableBitmapExtensions.FillEllipse(BitMap, item.X, item.Y, item.X + item.Radius * 2, item.Y + item.Radius * 2, item.Color);
-                    }));
-                }));
-            }
-
-            await Task.WhenAll(Task_List);
+            Task.Run(() => Sync_Rendering(Particle_List));
         }
 
         private void Sync_Rendering(List<PixelParticle> Particle_List)
@@ -104,26 +53,46 @@ namespace IdealGas_Simulator.ViewModels
                     WriteableBitmapExtensions.FillEllipse(BitMap, item.X, item.Y, item.X + item.Radius * 2, item.Y + item.Radius * 2, item.Color);
                 }
             }));
-
-            //Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Send, new Action(() => {
-            //    WriteableBitmapExtensions.DrawLineAa(BitMap, 1600, 100, 1600, 1400, Color.FromArgb(0xFF, 0xFF, 0xFF, 0xFF), 20);
-            //}));
-
-            //Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Send, new Action(() => {
-            //    WriteableBitmapExtensions.DrawLineAa(BitMap, 1600, 1400, 800, 1400, Color.FromArgb(0xFF, 0xFF, 0xFF, 0xFF), 20);
-            //}));
-
-            //Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Send, new Action(() => {
-            //    WriteableBitmapExtensions.DrawLineAa(BitMap, 800, 1400, 800, 100, Color.FromArgb(0xFF, 0xFF, 0xFF, 0xFF), 20);
-            //}));
-
-            //foreach (PixelParticle item in Collector) // Particles 그리기
-            //{
-            //    Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
-            //    {
-            //        WriteableBitmapExtensions.FillEllipse(BitMap, item.X, item.Y, item.X + item.Radius * 2, item.Y + item.Radius * 2, item.Color);
-            //    }));
-            //}
         }
+
+        //private async Task Async_Rendering(List<PixelParticle> Particle_List)
+        //{
+        //    List<PixelParticle> Collector = Particle_List.ToList();
+        //    List<Task> Task_List = new List<Task>() // Boundary 그리기
+        //    {
+        //        Task.Run(() => {
+        //            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Send, new Action(() => {
+        //                WriteableBitmapExtensions.DrawLineAa(BitMap, 800, 100, 1600, 100, Color.FromArgb(0xFF, 0xFF, 0xFF, 0xFF), 20);
+        //            }));
+        //        }),
+        //        Task.Run(() => {
+        //            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Send, new Action(() => {
+        //                WriteableBitmapExtensions.DrawLineAa(BitMap, 1600, 100, 1600, 1400, Color.FromArgb(0xFF, 0xFF, 0xFF, 0xFF), 20);
+        //            }));
+        //        }),
+        //        Task.Run(() => {
+        //            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Send, new Action(() => {
+        //                WriteableBitmapExtensions.DrawLineAa(BitMap, 1600, 1400, 800, 1400, Color.FromArgb(0xFF, 0xFF, 0xFF, 0xFF), 20);
+        //            }));
+        //        }),
+        //        Task.Run(() => {
+        //            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Send, new Action(() => {
+        //                WriteableBitmapExtensions.DrawLineAa(BitMap, 800, 1400, 800, 100, Color.FromArgb(0xFF, 0xFF, 0xFF, 0xFF), 20);
+        //            }));
+        //        }),
+        //    };
+
+        //    foreach (PixelParticle item in Collector) // Particles 그리기
+        //    {
+        //        Task_List.Add(Task.Run(() => {
+        //            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
+        //            {
+        //                WriteableBitmapExtensions.FillEllipse(BitMap, item.X, item.Y, item.X + item.Radius * 2, item.Y + item.Radius * 2, item.Color);
+        //            }));
+        //        }));
+        //    }
+
+        //    await Task.WhenAll(Task_List);
+        //}
     }
 }
--- PHYSICS/IdealGas_Simulator/ViewModels/MainViewModel_Simulation.cs
+++ PHYSICS/IdealGas_Simulator/ViewModels/MainViewModel_Simulation.cs
@@ -1,5 +1,4 @@
 ﻿using IdealGas_Simulator.Models;
-using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,34 +14,16 @@ namespace IdealGas_Simulator.ViewModels
 
         private void Simulation_Loop()
         {
-            //Random Random = new Random();
-
             int Last_X = 1200;
             int Last_Y = 750;
 
             int Entropy = 10;
-            int Particles_Number = 1000;
-
-            //Control_One_Particle(Last_X, Last_Y);
-
-            //Initialize_Particles(
-            //    new PixelParticle() {
-            //        Quantum_Handler = new Random(0), Color = Color.FromArgb(0xFF, 0x00, 0x00, 0xFF), Entropy = Entropy, Dimension = 2, Radius = 5, X = Last_X - 20, Y = Last_Y, X_Boundary_Min = 810, X_Boundary_Max = 1590, Y_Boundary_Min = 110, Y_Boundary_Max = 1390 },
-            //    new PixelParticle() {
-            //        Quantum_Handler = new Random(1), Color = Color.FromArgb(0xFF, 0x00, 0x00, 0xFF), Entropy = Entropy, Dimension = 2, Radius = 5, X = Last_X - 10, Y = Last_Y, X_Boundary_Min = 810, X_Boundary_Max = 1590, Y_Boundary_Min = 110, Y_Boundary_Max = 1390 },
-            //    new PixelParticle() {
-            //        Quantum_Handler = new Random(2), Color = Color.FromArgb(0xFF, 0x00, 0x00, 0xFF), Entropy = Entropy, Dimension = 2, Radius = 5, X = Last_X, Y = Last_Y, X_Boundary_Min = 810, X_Boundary_Max = 1590, Y_Boundary_Min = 110, Y_Boundary_Max = 1390 },
-            //    new PixelParticle() {
-            //        Quantum_Handler = new Random(3), Color = Color.FromArgb(0xFF, 0x00, 0x00, 0xFF), Entropy = Entropy, Dimension = 2, Radius = 5, X = Last_X - 10, Y = Last_Y, X_Boundary_Min = 810, X_Boundary_Max = 1590, Y_Boundary_Min = 110, Y_Boundary_Max = 1390 },
-            //    new PixelParticle() {
-            //        Quantum_Handler = new Random(4), Color = Color.FromArgb(0xFF, 0x00, 0x00, 0xFF), Entropy = Entropy, Dimension = 2, Radius = 5, X = Last_X + 20, Y = Last_Y, X_Boundary_Min = 810, X_Boundary_Max = 1590, Y_Boundary_Min = 110, Y_Boundary_Max = 1390 }
-            //);
+            int Particles_Number = 4400;
 
             PixelParticle[] Initial_Particles = new PixelParticle[Particles_Number];
             for (int i = 0; i < Initial_Particles.Length; i++)
             {
                 Initial_Particles[i] = new PixelParticle() {
-                    //Quantum_Handler = new Random(i),
                     Color = Color.FromArgb(0xFF, 0x00, 0x00, 0xFF),
                     Seed = i,
                     Entropy = Entropy,
@@ -50,10 +31,6 @@ namespace IdealGas_Simulator.ViewModels
                     Radius = 5,
                     X = Last_X,
                     Y = Last_Y,
-                    //X_Boundary_Min = 810,
-                    //X_Boundary_Max = 1590,
-                    //Y_Boundary_Min = 110,
-                    //Y_Boundary_Max = 1390,
                 };
             }
 
@@ -69,43 +46,6 @@ namespace IdealGas_Simulator.ViewModels
             while (true)
             {
                 Thread.Sleep(16);
-
-                //double Move_Radius = Random.Next(-Entropy, Entropy + 1);
-                //double Move_Radius_Decimal = Random.NextDouble();
-                //int Move_Radius_Decimal_Sign = Random.Next(0, 2);
-
-                //double Move_Azimuth = Random.Next(0, 361);
-                //double Move_Azimuth_Decimal = Random.NextDouble();
-                //int Move_Azimuth_Decimal_Sign = Random.Next(0, 2);
-
-                //Move_Radius += (double)(Move_Radius_Decimal * ((Move_Radius_Decimal_Sign == 1) ? 1 : -1));
-                //Move_Azimuth += (double)(Move_Azimuth_Decimal * ((Move_Azimuth_Decimal_Sign == 1) ? 1 : -1));
-
-                //// 일단 귀찮으니 사사오입 반올림 사용
-                //Last_X += (int)Math.Round(Move_Radius * Math.Sin(Move_Azimuth));
-                //Last_Y += (int)Math.Round(Move_Radius * Math.Cos(Move_Azimuth));
-
-
-                //// Boundary Condition
-                //if (Last_X < 810)
-                //{
-                //    Last_X = 810;
-                //}
-                //else if (Last_X > 1590)
-                //{
-                //    Last_X = 1590;
-                //}
-
-                //if (Last_Y < 110)
-                //{
-                //    Last_Y = 110;
-                //}
-                //else if (Last_Y > 1390)
-                //{
-                //    Last_Y = 1390;
-                //}
-
-                //Control_One_Particle(Last_X, Last_Y);
 
                 Control_Multi_Particles();
             }
@@ -123,33 +63,6 @@ namespace IdealGas_Simulator.ViewModels
 
             Pixel_Particles = Emitter;
         }
-
-        //private void Control_One_Particle(int x, int y)
-        //{
-        //    Task.Run(() => {
-        //        Initialize_Particles(
-        //            new PixelParticle() { Color = Color.FromArgb(0xFF, 0x00, 0x00, 0xFF), Radius = 5, X = x, Y = y, }
-        //        );
-
-        //        Drawing_Space_Object.Now_Rendering_Particles(Pixel_Particles);
-        //    });
-        //}
-
-        //private async void Control_Multi_Particles()
-        //{
-        //    List<Task> Task_List = new List<Task>();
-
-        //    foreach (PixelParticle item in Pixel_Particles) // 이건 비동기로 실행시키니 확률적인 게 안 되는 거 같은데
-        //    {
-        //        Task_List.Add(Task.Run(() => {
-        //            item.Invoke_Probability_Position();
-        //        }));
-        //    }
-
-        //    await Task.WhenAll(Task_List);
-
-        //    Drawing_Space_Object.Now_Rendering_Particles(Pixel_Particles);
-        //}
 
         private async void Control_Multi_Particles()
         {
